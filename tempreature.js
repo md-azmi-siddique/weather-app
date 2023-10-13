618d548bfa41a49d94f8d717f37858183ae174e4
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-10-13 09:02:47 +0600

    weather app using api

---
+++ tempreature.js
@@ -0,0 +1,27 @@
+const apiKey = "1ca9f7d3281f37def411f8dde39f692a"; // Replace with your actual API key
+const button = document.getElementById("search");
+
+button.addEventListener("click", searchTemp);
+
+async function searchTemp() {
+  try {
+    console.log("hit");
+    const cityName = document.getElementById("city-name").value; // Use parentheses, not square brackets
+    if (!cityName) {
+      alert("input a city name");
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    displayTemp(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const displayTemp = (data) => {
+  console.log(data);
+  document.getElementById("city").innerText = data.name;
+  document.getElementById("temp").innerText = data.main.temp;
+  document.getElementById("condition").innerText = data.weather[0].main;
+};
